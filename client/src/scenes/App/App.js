4ab01ed67722a0998459bf2db89cb1efc69a9c10
David Higuita <david.higuita.75@gmail.com>
2017-08-07 22:02:35 -0500

    Added new table component to App scene

--- client/src/scenes/App/App.js
+++ client/src/scenes/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import DataTable from 'react-md/lib/DataTables/DataTable'
 import TableOrdersContainer from '../../components/TableOrders'
 
 class App extends Component {
